b40d16d1258eef8c4a0b0d33345a3146f042fe29
Joe Constant <joe@joeconstant.com>
2025-08-17 22:29:52 -0600

    wip

---
+++ keyboards/crkbd/keymaps/lazyguru/rules.mk
@@ -0,0 +1,6 @@
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+BACKLIGHT_ENABLE = no      # Specify the backlights pin in config.h
+RGBLIGHT_ENABLE = no       # Addressable RGB backlights.
+RGB_MATRIX_ENABLE = no    # There is an RGB LED on every key.
+#lVIA_ENABLE      = yes    # Enable VIA
